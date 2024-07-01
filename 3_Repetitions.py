21867d7d0a19fd36ec701cc0ef8ba81aa42440f2
jackyyoyo2@gmail.com <jackyyoyo2@gmail.com>
2024-07-01 16:35:37 +0800

    q3 solution

--- 3_Repetitions.py
+++ 3_Repetitions.py
@@ -3,16 +3,12 @@ max_length = 1
 string_list = input().strip()
 
 temp_len = 0
-for i in range(len(string_list) - 1):
-
-    if string_list[i] == string_list[i + 1] and temp_len == 0:
-        temp_len += 2
-    elif string_list[i] == string_list[i + 1]:
+cur = 'A'
+for char in string_list:
+    if char == cur:
         temp_len += 1
+        max_length = max(max_length, temp_len)
     else:
-        temp_len = 0
-
-    max_length = max(max_length, temp_len)
-    
-
+        cur = char
+        temp_len = 1
 print(max_length)
